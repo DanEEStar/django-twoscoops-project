b6ced7d17d94d40fdc56fbe83eb18afe6d54fe1c
Daniel Egger <daniel.egger@gmail.com>
2013-02-27 11:40:33 +0100

    compass hinzugefügt

---
+++ compass/config/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../../src/{{ project_name }}/core/static/css"
+sass_dir = "sass"
+images_dir = "../../src/{{ project_name }}/core/static/img"
+javascripts_dir = "../../src/{{ project_name }}/core/static/js"
+output_style = :compressed
+line_comments = false
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
