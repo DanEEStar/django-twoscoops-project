3e9c0c9b5570ecc5ef31866edcbe6d2a3b9ac28a
maersu <me@maersu.ch>
2014-02-04 17:35:12 +0100

    added glyphicons pro

--- src/project_name/apps/core/urls.py
+++ src/project_name/apps/core/urls.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^glyphicons/$', TemplateView.as_view(template_name='glyphicons.html')),
 )
 
